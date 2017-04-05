58b406cbf1ca6412bb1ed66628ec5e2b833696fb
Adam <adam8690@hotmail.co.uk>
2017-04-05 17:30:25 +0100

    wrote method to concat the arrays and passed test.

--- array_tasks.js
+++ array_tasks.js
@@ -1,8 +1,11 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
-
-	// },
+	concat: function (arr1, arr2) {
+		for(i = 0; i < arr2.length; i++){
+		arr1.push(arr2[i]);
+		}
+		return arr1;
+	},
 
 	// insertAt: function (arr, itemToAdd, index) {
 		
